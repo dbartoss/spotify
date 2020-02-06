735504d854b7f175777283259129af8a7aa288c0
dbartoss <dawid.rafael.bartosz@gmail.com>
2020-02-06 20:30:51 +0100

    feat(App.xaml): Set color resource dictionary to create custom dark theme

--- Spotify.WPF/MainWindow.xaml.cs
+++ Spotify.WPF/MainWindow.xaml.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MaterialDesignColors;
-using MaterialDesignThemes.Wpf;
 
 namespace spotify
 {
@@ -24,28 +10,7 @@ namespace spotify
     {
         public MainWindow()
         {            
-            var paletteHelper = new PaletteHelper();
-            //Retrieve the app's existing theme
-            ITheme theme = paletteHelper.GetTheme();
-
-            //Change the base theme to Dark
-            theme.SetBaseTheme(Theme.Dark);
-            //or theme.SetBaseTheme(Theme.Light);
-
-            //Change all of the primary colors to Red
-            theme.SetPrimaryColor(Colors.Lime);
-
-            //Change all of the secondary colors to Blue
-            theme.SetSecondaryColor(Colors.Green);
-
-            //You can also change a single color on the theme, and optionally set the corresponding foreground color
-            theme.PrimaryMid = new ColorPair(Colors.LimeGreen, Colors.White);
-
-            //Change the app's current theme
-            paletteHelper.SetTheme(theme);
-
             InitializeComponent();
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
